812d45ddf4c370f7f8e26ef8df7125dd67c2c47e
tiagosantos-dev <tiagoltj@hotmail.com>
2022-03-11 22:40:08 -0300

    feat(create): Create application patient-ditial

---
+++ patient-digital/src/main/java/com/ts/patientdigital/PatientDigitalApplication.java
@@ -0,0 +1,13 @@
+package com.ts.patientdigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientDigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientDigitalApplication.class, args);
+	}
+
+}
---
+++ patient-digital/src/test/java/com/ts/patientdigital/PatientDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ts.patientdigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientDigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
